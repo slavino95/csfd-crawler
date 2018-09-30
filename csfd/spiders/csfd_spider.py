8c3a0ba836d94ae1561a99b87c0db25416630a62
slavino95 <slavino111@gmail.com>
2018-09-30 21:10:38 +0200

    minor changes

--- csfd/spiders/csfd_spider.py
+++ csfd/spiders/csfd_spider.py
@@ -15,11 +15,12 @@ class CsfdSpider(scrapy.Spider):
     def parse_movie(self, response):
     	def extract_with_css(query):
             return response.css(query).extract_first().strip()
-    		
+            
     	yield {
 			'movie': extract_with_css('h1::text'),
 			'genre': extract_with_css('p.genre::text'),
 			'origin': extract_with_css('p.origin::text'),
 			'year': extract_with_css('span[itemprop="dateCreated"]::text'),	
 			'length': response.css('p.origin::text').extract()[1],
+            'rating': extract_rating('h2.average::text'),
     	}
