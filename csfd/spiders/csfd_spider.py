f84ba58666d13b57d053ff18ccdefd60fba6dd53
Slavino <slavino111@gmail.com>
2018-09-30 19:40:30 +0200

    Initial commit

---
+++ csfd/spiders/csfd_spider.py
@@ -0,0 +1,25 @@
+import scrapy
+
+class CsfdSpider(scrapy.Spider):
+    name = 'csfd'
+
+    start_urls = ['https://www.csfd.cz/filmy-online/']
+
+    def parse(self, response):
+    	for movie in response.css('div.name a::attr(href)'):
+    		yield response.follow(movie, self.parse_movie)
+
+    	for href in response.css('a.next::attr(href)'):
+            yield response.follow(href, self.parse)	
+
+    def parse_movie(self, response):
+    	def extract_with_css(query):
+            return response.css(query).extract_first().strip()
+    		
+    	yield {
+			'movie': extract_with_css('h1::text'),
+			'genre': extract_with_css('p.genre::text'),
+			'origin': extract_with_css('p.origin::text'),
+			'year': extract_with_css('span[itemprop="dateCreated"]::text'),	
+			'length': response.css('p.origin::text').extract()[1],
+    	}
